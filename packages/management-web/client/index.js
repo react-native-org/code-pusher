79bf11ea2a723914f10987da0ac4ba3049fac588
jh3r <jay.m.hu@newegg.com>
2018-04-06 11:01:32 +0800

    Support antd

--- packages/management-web/client/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill';
-
-import { createApp, createAppStore } from '../src';
-import { hydrate, render } from 'react-dom';
-
-const store = createAppStore();
-const App = createApp(store);
-const h = window.__INITIAL_STATE__ ? hydrate : render;
-h(App, document.getElementById('app'));
--- packages/management-web/src/App.js
+++ packages/management-web/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
+import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Reafdsaaact</h1>
         </header>
         <p className="App-intro">
           To get started, efdfdsadit <code>src/App.js</code> and save to relofdsfdsafad.
+          <Button>XXXX</Button>
         </p>
       </div>
     );
