e00c48d2eb1fc5ee36212f6343b03d65f5f8c513
jh3r <jay.m.hu@newegg.com>
2018-04-11 18:07:14 +0800

    UI update

--- packages/management-web/config-overrides.js
+++ packages/management-web/config-overrides.js
@@ -2,7 +2,6 @@ const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  console.log(config, env);
   config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
   config = injectBabelPlugin(['transform-decorators-legacy'], config);
   config = rewireLess.withLoaderOptions({
--- packages/management-web/src/pages/app-list/AppListPage.js
+++ packages/management-web/src/pages/app-list/AppListPage.js
@@ -1,7 +1,38 @@
+import './AppListPage.less';
+
+import { Button, Card, Col, Icon, List, Row } from 'antd';
 import React, { PureComponent } from 'react';
 
 export class AppListPage extends PureComponent {
+  state = {
+    loading: false
+  };
   render() {
-    return <div>AppListPage</div>;
+    const list = [1, 2, 3];
+    return (
+      <div className="page-app-list">
+        {/* 搜索栏 */}
+        <Row>
+          <Col span={10}>xxx</Col>
+        </Row>
+        <List
+          rowKey="id"
+          loading={this.state.loading}
+          grid={{ gutter: 24, lg: 4, md: 3, sm: 2, xs: 1 }}
+          dataSource={[...list]}
+          renderItem={item => (
+            <List.Item key={item.id}>
+              <Card hoverable actions={[<a key="op1">操作一</a>, <a key="op2">操作二</a>]}>
+                <Card.Meta
+                  avatar={<img alt="" src={item.avatar} />}
+                  title={<a href="#">{item.title}</a>}
+                  description={<p>{item.description}</p>}
+                />
+              </Card>
+            </List.Item>
+          )}
+        />
+      </div>
+    );
   }
 }
